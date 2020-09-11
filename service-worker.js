36aaefde12a187e09c2201f1d43871efdd87e4d3
NamPhamHoang <phamhoangnam55@gmail.com>
2020-09-11 12:38:07 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Form-strela/precache-manifest.0c72148209b00d82f5858dc591441cc3.js"
+  "/Form-strela/precache-manifest.fd67587cf8aaeee2ff96fb22a2f2e2c2.js"
 );
 
 self.addEventListener('message', (event) => {
